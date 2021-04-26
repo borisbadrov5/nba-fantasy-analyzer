e345850ec8b5cc5886fb6616dad91094e4264ae3
boris <boris.badrov@gmail.com>
2021-04-26 21:44:31 +0800

    heroku uses `postgres://` which isnt supported by sqlalchemy :mad:

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config(object):
-    DATABASE_URL = os.environ.get('DATABASE_URL')
+    DATABASE_URL = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
