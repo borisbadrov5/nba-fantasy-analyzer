eefddc13933d52ff714cf603723227f67936fb87
boris <boris.badrov@gmail.com>
2021-04-26 10:12:56 +0800

    use heroku port

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,6 @@
+import os
+
 from api.app import app
+
 if __name__ == '__main__':
-    app.run(debug = True, host = '0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get("PORT", 5000))
